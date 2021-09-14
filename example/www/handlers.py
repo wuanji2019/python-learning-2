a0d08e8ad07709656e327a184b1279aa86d967f9
anji <331531027@qq.com>
2021-09-14 16:44:12 +0800

    📝 day 07

--- example/www/handlers.py
+++ example/www/handlers.py
@@ -3,3 +3,15 @@
 
 
 ' url handlers '
+
+from coroweb import get, post
+
+from models import User, Comment, Blog
+
+@get('/')
+async def index(request):
+  users = await User.findAll()
+  return {
+      '__template__': 'test.html',
+      'users': users
+  }
